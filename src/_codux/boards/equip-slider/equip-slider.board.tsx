b7d0fdd885aea58172d74b2020de0322df8eb94b
SIROFSIN <ksmirnov23@gmail.com>
2025-10-13 20:20:26 +0300

    КОММИТ ДЛЯ ФИКСАЦИИ ПОСЛЕДНЕЙ ВЕРСИИ (СЕРТИФИЦИРОВАННОЙ)

--- src/_codux/boards/equip-slider/equip-slider.board.tsx
+++
@@ -1,7 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { Equip } from '../../../components/equip/equip';
-
-export default createBoard({
-    name: 'EquipSlider',
-    Board: () => <Equip />
-});
